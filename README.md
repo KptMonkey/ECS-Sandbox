f0f2a4e4287554368374f006d8e1e35b60198153
Alex <kpt.monkey@googlemail.com>
2019-09-13 21:30:49 +0200

    Add ComponentArray and update Readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ECS-Sandbox
-Simple ECS implementation to fool around and learn
+Simple ECS implementation based on the post from austinmorlan.com to fool around and learn.
---
+++ component_array.hpp
@@ -0,0 +1,65 @@
+#include "entity.hpp"
+#include <array>
+#include <unordered_map>
+
+class IComponentArray {
+public:
+    virtual ~IComponentArray() = default;
+    virtual void entityDestroyed(Entity e) = 0;
+};
+
+template<typename T>
+class ComponentArray : public IComponentArray
+{
+public:
+    void insertData(Entity e, T component) {
+       int newIndex = mSize;
+       mEntityToIndexMap[e] = newIndex;
+       mIndexToEntityMap[newIndex] = e;
+       mComponentArray[newIndex] = component;
+       ++mSize;
+    }
+
+    void removeData(Entity e) {
+        auto indexOfRemovedEntity = mEntityToIndexMap[e];
+        auto indexOfLastElement = mSize - 1;
+        mComponentArray[indexOfRemovedEntity] = mComponentArray[indexOfLastElement];
+
+        Entity entityOfLastElement = mIndexToEntityMap[indexOfLastElement];
+        mEntityToIndexMap[entityOfLastElement] = indexOfRemovedEntity;
+        mIndexToEntityMap[indexOfRemovedEntity] = entityOfLastElement;
+
+        mEntityToIndexMap.erase(entity);
+        mIndexToEntityMap.erase(indexOfLastElement);
+
+        --mSize;
+    }
+
+    T& GetData(Entity entity)
+    {
+        assert(mEntityToIndexMap.find(entity) != mEntityToIndexMap.end() && "Retrieving non-existent component.");
+
+        // Return a reference to the entity's component
+        return mComponentArray[mEntityToIndexMap[entity]];
+    }
+
+    void EntityDestroyed(Entity entity) override
+    {
+        if (mEntityToIndexMap.find(entity) != mEntityToIndexMap.end())
+        {
+            // Remove the entity's component if it existed
+            RemoveData(entity);
+        }
+    }
+
+private:
+
+    int mSize = 0;
+    std::array<T, MAX_ENTITIES> mComponentArray;
+
+    // Map from an entity ID to an array index.
+    std::unordered_map<Entity, int> mEntityToIndexMap;
+
+    // Map from an array index to an entity ID.
+    std::unordered_map<int, Entity> mIndexToEntityMap;
+};
